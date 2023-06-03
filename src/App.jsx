c72f66084c5590e1e154f52441717ad087163205
JoeyG214 <erjo.guce214@gmail.com>
2023-06-03 14:46:00 -0400

    Created DroneInfo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Header from "./components/Header"
 import CaptureButton from "./components/CaptureButton"
 import Footer from "./components/Footer"
+import DroneInfo from "./components/DroneInfo"
 
 const App = () => {
   return (
-    <main className="flex flex-col w-screen min-h-screen bg-gray-900">
+    <main className="flex flex-col justify-center w-screen min-h-screen bg-gray-900">
       <Header />
       <section className="flex-grow">
         <CaptureButton />
+        <DroneInfo />
       </section>
       <Footer />
     </main>
---
+++ src/components/DroneInfo.jsx
@@ -0,0 +1,14 @@
+const DroneInfo = () => {
+  return (
+    <section className="flex flex-col items-center my-8">
+      <h2 className="text-white text-2xl font-bold mb-4">
+        Drone Data
+      </h2>
+      <div className="text-white text-lg font-medium px-6 md:px-12">
+
+      </div>
+    </section>
+  )
+}
+
+export default DroneInfo
