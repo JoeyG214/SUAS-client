2eed28851b3bb93be0c9b51c18e1561aedfde1fd
JoeyG214 <erjo.guce214@gmail.com>
2023-06-15 19:08:15 -0400

    Modified event handler

--- src/components/CaptureButton.jsx
+++ src/components/CaptureButton.jsx
@@ -1,13 +1,9 @@
-const CaptureButton = () => {
-const handleClick = () => {
-
-}
-
+const CaptureButton = ({ handleClick }) => {
   return (
     <div className="flex">
       <button
         className="w-screen bg-neonGreen text-white py-2 px-4 mx-12 my-4 rounded shadow-neon"
-        onClick={(handleClick)}
+        onClick={handleClick}
       >
         <span className="text-2xl font-bold">
           Capture Image
