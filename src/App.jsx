e39c2c5643bc65dc1773c235788a88d8e7b980bf
JoeyG214 <erjo.guce214@gmail.com>
2023-06-15 19:07:09 -0400

    Added image fetching function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import Header from "./components/Header"
-import CaptureButton from "./components/CaptureButton"
-import Footer from "./components/Footer"
-import DroneInfo from "./components/DroneInfo"
-import CapturedImages from "./components/CapturedImages"
+import Header from './components/Header'
+import CaptureButton from './components/CaptureButton'
+import Footer from './components/Footer'
+import DroneInfo from './components/DroneInfo'
+import CapturedImages from './components/CapturedImages'
+
+import { useState } from 'react'
+import imageDataService from './services/scripts/ImageData'
 
 const App = () => {
+  const [imageUrl, setImageUrl] = useState(null)
+  const [imageUrls, setImageUrls] = useState([])
+
+  const fetchImage = async () => {
+    try {
+      const image = imageDataService.capture()
+      const imageUrl = URL.createObjectURL(image)
+
+      setImageUrl(imageUrl)
+      setImagesUrls(imageUrls.concat(imageUrl))
+    } catch (error) {
+      console.error(`Failed to fetch image: ${error}`)
+    }
+  }
+  
   return (
     <main className="flex flex-col justify-center w-screen min-h-screen bg-gray-900">
       <Header />
       <section className="flex-grow">
-        <CaptureButton />
+        <CaptureButton handleClick={fetchImage} />
         <DroneInfo />
-        <CapturedImages />
+        <CapturedImages images={imageUrls} />
       </section>
       <Footer />
     </main>
