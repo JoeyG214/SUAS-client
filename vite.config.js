f8b3c492fb58095d470a49a3338e6036e826f9d0
JoeyG214 <erjo.guce214@gmail.com>
2023-06-15 14:02:08 -0400

    added proxy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        // API Server Address in development environment
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      }
+    }
+  }
 })
